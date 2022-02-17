9bfc60158aa1fccbd0033bcc7146922d861b380a
zer0-x <65136727+ZER0-X@users.noreply.github.com>
2022-02-17 03:21:56 +0300

    Add more telegram types & change how they work

--- telegram2rss/telegram_types.py
+++ telegram2rss/telegram_types.py
@@ -1,21 +1,63 @@
-"""Telegram messages types and how to scrape them."""
+"""Telegram data types and how to scrape them."""
 
 
-class Type:
+class MessageType:
     """Type class to make messages types objects."""
 
-    def __init__(self, name: str, html_class: str, emoji: str):
+    def __init__(self, name: str, selector: str, emoji: str):
         """Get type information."""
         self.name = name
-        self.html_class = html_class
+        self.selector = selector
         self.emoji = emoji
 
 
-TEXT = Type("text", ".tgme_widget_message_text", "📃")
-PHOTO = Type("image", ".tgme_widget_message_photo_wrap", "📷")
-VIDEO = Type("video", ".tgme_widget_message_video", "📹")
-VOICE = Type("voice", ".tgme_widget_message_voice", "🎤")
-DOCUMENT = Type("document", ".tgme_widget_message_document", "📎")
-LOCATION = Type("location", ".tgme_widget_message_location", "📍")
-POLL = Type("poll", ".tgme_widget_message_poll", "📊")
-STICKER = Type("sticker", ".tgme_widget_message_sticker", "🖼️️")
+TEXT = MessageType("text", ".tgme_widget_message_text", "📃")
+PHOTO = MessageType("image", ".tgme_widget_message_photo_wrap", "📷")
+VIDEO = MessageType("video", ".tgme_widget_message_video_player", "📹")
+VOICE = MessageType("voice", ".tgme_widget_message_voice_player", "🎤")
+DOCUMENT = MessageType("document", ".tgme_widget_message_document_wrap", "📎")
+LOCATION = MessageType("location", ".tgme_widget_message_location", "📍")
+POLL = MessageType("poll", ".tgme_widget_message_poll", "📊")
+STICKER = MessageType("sticker", ".tgme_widget_message_sticker", "🖼️️")
+
+
+class MetaType:
+    """Type class to make meta data types objects."""
+
+    def __init__(self, name: str, selector: str):
+        """Get type information."""
+        self.name = name
+        self.selector = selector
+
+
+CHANNEL_TITLE = MetaType("channel_title", ".tgme_channel_info_header_title")
+CHANNEL_DESCRIPTION = MetaType("channel_description", ".tgme_channel_info_description")
+CHANNEL_IMAGE = MetaType("channel_description", ".tgme_page_photo_image img")
+
+MESSAGE_AUTHOR = MetaType("author", ".tgme_widget_message_author")
+MESSAGE_DATE = MetaType("date", ".tgme_widget_message_date time")
+MESSAGE_VIEWS = MetaType("views", ".tgme_widget_message_views")
+MESSAGE_NUMBER = MetaType("url", ".tgme_widget_message_date")
+
+VIDEO_DURATION = MetaType("video_duration", ".tgme_widget_message_video_player time")
+VIDEO_THUMB = MetaType("video_thumbnail", ".tgme_widget_message_video_thumb")
+
+VOICE_URL = MetaType("voice_url", ".tgme_widget_message_voice audio")
+VOICE_DURATION = MetaType(
+    "voice_duration",
+    ".tgme_widget_message_voice_duration",
+)
+
+DOCUMENT_TITLE = MetaType("document_title", ".tgme_widget_message_document_title")
+DOCUMENT_SIZE = MetaType("document_size", ".tgme_widget_message_document_extra")
+
+POLL_QUESTION = MetaType("poll_question", ".tgme_widget_message_poll_question")
+POLL_TYPE = MetaType("poll_type", ".tgme_widget_message_poll_type")
+POLL_OPTIONS = MetaType("poll_options", ".tgme_widget_message_poll_options")
+POLL_OPTION_PERCENT = MetaType(
+    "poll_option_percent", ".tgme_widget_message_poll_option_percent"
+)
+POLL_OPTION_VALUE = MetaType(
+    "poll_option_value",
+    ".tgme_widget_message_poll_option_value .tgme_widget_message_poll_option_text",
+)
