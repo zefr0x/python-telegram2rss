eb95ea9fd597c4415128042b9aa7a71658cbc8e7
zer0-x <65136727+ZER0-X@users.noreply.github.com>
2022-03-09 11:01:21 +0300

    Add tests for fetch_to_python method

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,2 +1,22 @@
 """Unit testing with pytest."""
 import pytest
+
+import telegram2rss
+
+
+def test_TGChannel():
+    """Create object and use fetch_to_python method."""
+    channel = telegram2rss.TGChannel(input("Test channel id: "))
+
+    data = channel.fetch_to_python(13)
+
+    print(data)
+
+    assert channel.channel_title
+    assert channel.channel_description
+    assert channel.channel_image_url is not None
+    assert channel.channel_subscribers_count is not None
+    assert channel.channel_photos_count is not None
+    assert channel.channel_videos_count is not None
+    assert channel.channel_files_count is not None
+    assert channel.channel_url.startswith(telegram2rss.TELEGRAM_URL)
